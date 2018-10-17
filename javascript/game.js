edbc93c08288b8e66b4a0607db2892cacb477c1e
Rebecca Watson <rebecca.watson2125@gmail.com>
2018-10-17 17:22:31 -0600

    Added first three working buttons

---
+++ javascript/game.js
@@ -0,0 +1,55 @@
+function increment(name) {
+    var number = parseInt(name.innerHTML.split("/",1));
+    var storage = parseInt(name.innerHTML.split("/")[1]);
+    if (number < storage){
+	  	number++;
+  	  	name.innerHTML = number + "/" + storage;
+    }
+}
+
+function decrement(name) {
+    var number = name.innerHTML.split("/",1);
+    if (number > 0){
+    	var storage = name.innerHTML.split("/")[1];
+    	number--;
+    	name.innerHTML = number + "/" + storage;
+    }
+}
+
+function storage(name,x) {
+    var price = Number(document.getElementById(x+"StorUp").innerText.split("$")[1]);
+	if (Number(money.innerHTML) >= price) {
+    	var number = name.innerHTML.split("/",1);
+    	var storage = name.innerHTML.split("/")[1];
+        var total = Number(money.innerHTML);
+    	storage=storage*2;
+    	name.innerHTML = number + "/" + storage;
+        total = total - price
+        money.innerHTML = total.toFixed(2);
+    }
+}
+
+function auto(name) {
+	setInterval(function(){increment(name);},10000);
+}
+
+function incdec(a,b){
+	if (b.innerHTML.split("/",1) > 0 && parseInt(a.innerHTML.split("/",1)) < parseInt(a.innerHTML.split("/")[1])) {
+	    increment(a)
+        decrement(b)
+    }
+}
+
+function auto2(a,b) {
+	setInterval(function(){incdec(a,b);},10000);
+}
+
+function sell(name,x){
+	if (parseInt(name.innerHTML.split("/",1)) > 0){
+		decrement(name)
+    	var total = Number(money.innerHTML);
+    	var price = Number(document.getElementById(x+"Sell").innerText.split("$")[1]);
+    	total += price;
+    	money.innerHTML = total.toFixed(2);
+    }
+}
